16093d04d1a455e0a1817829d637e0840baaedcb
ruslazo <139565075+ruslazo@users.noreply.github.com>
2025-03-03 17:30:29 +0100

    ec2-micro-frankfurt

--- main.tf
+++ main.tf
@@ -11,10 +11,14 @@ data "aws_ami" "debian_ami" {
   owners = ["136693071363"] # Debian
 }
 
+provider "aws" {
+  region = "eu-central-1"  # Replace with your desired region
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.debian_ami.id
   instance_type = var.instance_type
   tags = {
-    Name = "DebianInstance"
+    Name = "debian-instance"
   }
 }
