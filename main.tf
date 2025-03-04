e23b612cdc1d6cbca8da2b9d0e4f829dd72efff0
ruslazo <139565075+ruslazo@users.noreply.github.com>
2025-03-04 14:01:58 +0100

    reqion-eu-central

--- main.tf
+++ main.tf
@@ -11,11 +11,8 @@ data "aws_ami" "debian_ami" {
   owners = ["136693071363"] # Debian
 }
 
-provider "aws" {
-  region = "eu-central-1"  # Replace with your desired region
-}
-
 resource "aws_instance" "web" {
+  region = "eu-central-1"  # Replace with your desired region
   ami           = data.aws_ami.debian_ami.id
   instance_type = var.instance_type
   tags = {
