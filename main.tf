4107a8d381de45c9b4e2c87700273ed8e4066744
ruslazo <139565075+ruslazo@users.noreply.github.com>
2025-03-12 18:57:34 +0100

    Update main.tf - sg_http_own_ip_in

--- main.tf
+++ main.tf
@@ -11,10 +11,29 @@ data "aws_ami" "debian_ami" {
   owners = ["136693071363"] # Debian
 }
 
-resource "aws_instance" "web" {  
+data "aws.vpc" "default" {
+  default = true
+} 
+resource "aws_instance" "freq" {  
   ami           = data.aws_ami.debian_ami.id
   instance_type = var.instance_type
   tags = {
-    Name = "debian-instance"
+    Name = "debian-instance-freq"
   }
+}
+
+resource "aws_security_group" "freq"{
+  name = "freq"
+  description = "Allow http and https in from inet 192.168.254.128/24: IPv4 address for the Ethernet interface with a /24 subnet mask.Allow everything out"
+vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "freq_http_in" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_block = [192.168.254.128/24]
+
+  security_group.id = aws_security_group.freq.id
 }
